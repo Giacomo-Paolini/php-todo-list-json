df921f9165d4f8d98c1bbb0529f56730e6a3315e
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-06-12 17:01:13 +0200

    json

--- api.php
+++ api.php
@@ -1,11 +1,18 @@
-<?php $names = [
-        [
-            "name" => "Mario",
-            "last_name" => "Rossi"
-        ],
-        [
-            "name" => "Giovanna",
-            "last_name" => "Bianchi"
-        ]
+<?php
+header('Content-Type: application/json');
+
+$names = [
+    [
+        "name" => "Mario",
+        "last_name" => "Rossi"
+    ],
+    [
+        "name" => "Giovanna",
+        "last_name" => "Bianchi"
     ]
+];
+
+$arrayNames = json_encode($names);
+
+echo $arrayNames;
 ?>
