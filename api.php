2f63eda90d83465514dd598cc4a0deeec29e2dc9
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-06-12 16:22:52 +0200

    first commit

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php $names = [
+        [
+            "name" => "Mario",
+            "last_name" => "Rossi"
+        ],
+        [
+            "name" => "Giovanna",
+            "last_name" => "Bianchi"
+        ]
+    ]
+?>
---
+++ main.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+        console.log('prova')
+    }
+}).mount('#app')
